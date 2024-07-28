45367743271f15a000228fcbcbf6498b2c5df468
Ian <iancheif117@gmail.com>
2024-07-28 09:56:36 +0800

    Fixed Swagger Autogeneration

--- BED_Assignment/app.js
+++ BED_Assignment/app.js
@@ -32,15 +32,13 @@ import highscoreController from "./controllers/highscoreController";
 import validateEvent from "./middleware/validateEvent";
 import validateGame from "./middleware/validateGame";
 
-import swaggerUi from "swagger-ui-express";
-const swaggerDocument = require("./swagger-output.json"); // Import generated spec
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger-output.json");
 
 const app = express();
 const jwt = require('jsonwebtoken');
 const PORT = process.env.PORT || 3001;
 const staticMiddleware = express.static("./public"); // Path to the public folder
-
-
 
 //const multer = require('multer');
 const fs = require('fs');
@@ -55,81 +53,10 @@ app.use(bodyParser.json({ limit: '400mb' }));
 app.use(bodyParser.urlencoded({ limit: '400mb', extended: true }));
 app.use(staticMiddleware);
 
-
-// Routes
-//Routes for users
-app.get("/users", userController.getAllUsers);
-app.get("/users/:userId", userController.getUserByUserId);
-app.post("/users", validateUser, userController.createUser);
-app.put("/users/:userId", validateUser, userController.updateUser);
-app.delete("/users/:userId", userController.deleteUserById);
-
-
-//Routes for user login to login user
-app.post('/userlogin', userController.loginUser);
-app.post('/companylogin', userController.loginUser);
-app.post('/adminlogin', adminController.loginUser);
-
-
-app.post('/createadmin', adminController.createAdminUser);
-
-
-//Routes for events
-app.post('/create-event', authenticateToken, validateEvent, EventController.createEvent);
-
-// Serve protected.html for /events route, i used this for the js to authenticate before serving
-app.get('/events', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/html/protected.html'));
-});
-// myEvents.html page
-app.get('/api/events/userEvents', authenticateToken, EventController.getEventsByUserId);
-
-app.get('/myEvents', async(req, res) => {res.sendFile(path.join(__dirname + "/public/html/myEvents.html"))});
-
-// events-content is the route for the events page, so basically after protected.html is served, the js will fetch the events-content with the token, the middleware can then authenticate it.
-app.get('/events-content', specialAuthenticateToken, EventController.serveEventsContent);
-
-app.post('/api/:eventId/signup', authenticateToken, EventController.signUserUp);
-
-app.get("/api/events", EventController.getAllEvents);
-app.get("/api/events/:eventId", EventController.getEventById);
-app.post('/api/events', authenticateToken, validateEvent, EventController.createEvent);
-app.patch('/api/:eventId/leave', authenticateToken, EventController.updateEventAttendance);
-app.delete("/api/events/:eventId/kick/:userId", authenticateToken, (req, res, next) => {
-    
-    EventController.updateEventAttendance(req, res, next);
-  });
-app.delete("/api/events/:eventId", authenticateToken, EventController.deleteEvent);
-
-//paypal stuff
-app.post('/api/events/:eventId/authorize', authenticateToken, eventPaymentController.authorizePayment);
-app.post('/api/events/:eventId/capture', authenticateToken, eventPaymentController.capturePayment);
-app.put('/api/payments/capture/:eventId', authenticateToken, eventPaymentController.capturePayment);
-
-// refreshToken Routes
-app.delete('/refreshToken', (req, res) => { refreshToken.removeToken(req, res) });
-app.get("/refreshToken", async(req, res) => {  refreshToken.refreshToken(req, res)});    
-app.post("/refreshToken", async(req, res) => {refreshToken.addToken(req, res)});
-
-
-// Game routes
-app.get('/game', async(req, res) => { res.sendFile(path.join(__dirname + "/public/html/game.html")) });
-
-app.post('/save-game', authenticateToken,validateGame, gameController.saveGame);
-app.post('/save-game2', authenticateToken, validateGame, gameController.saveGame2);
-app.get('/get-games', authenticateToken, gameController.getGames);
-
-// Highscore routes
-app.get('/get-high-scores', authenticateToken, highscoreController.getHighScores);
-app.post('/create-high-score', authenticateToken, highscoreController.createHighScore);
-//Ends here
-
+// Routes for html page
 app.get("/signup", async(req, res) => {
     res.sendFile(path.join(__dirname + "/public/html/signup.html"));
 });
-
-//Route for user sign up
-app.post("/signup", userController.createUser);
 
 //Route for user login
 app.get("/login", async(req, res) => {
@@ -199,48 +126,6 @@ app.get("/credits", async(req, res) =>{
     res.sendFile(path.join(__dirname + "/public/html/credits.html"))
 });
 
-
-
-// Backend routes for comments
-app.get("/api/article/comment/:commentId", articleCommentController.getArticleCommentById);
-app.get("/api/article/:articleId/comments/latest", articleCommentController.getAllCommentsFromArticleIdByLatest);
-app.get("/api/article/:articleId/comments/relevance", articleCommentController.getAllCommentsFromArticleIdByRelevance);
-app.post("/api/article/:articleId/comments", validateComment, articleCommentController.createArticleComment);
-app.put("/api/article/:articleId/comments", validateComment, articleCommentController.updateArticleCommentContent);
-app.delete("/api/article/:articleId/comments", articleCommentController.deleteArticleComment);
-
-app.get("/api/event/comment/:commentId", eventCommentController.getEventCommentById);
-app.get("/api/event/:eventId/comments/latest", eventCommentController.getAllCommentsFromEventIdByLatest);
-app.get("/api/event/:eventId/comments/relevance", eventCommentController.getAllCommentsFromEventIdByRelevance);
-app.post("/api/event/:eventId/comments", validateComment, eventCommentController.createEventComment);
-app.put("/api/event/:eventId/comments", validateComment, eventCommentController.updateEventCommentContent);
-app.delete("/api/event/:eventId/comments", eventCommentController.deleteEventComment);
-
-// Backend routes for chatbot
-app.get("/api/chatbot/:conversationId", authenticateToken,  chatBotController.fetchChatHistory);
-app.post("/api/chatbot/:conversationId", authenticateToken, validateAddChatHistory, chatBotController.postUserInput);
-
-app.get("/api/chatConversation/:userId", authenticateToken, chatBotController.fetchChatConversationsByUserId);
-app.post("/api/chatConversation/:userId", validateEditConversationTitle, chatBotController.addNewConversation);
-app.put("/api/chatConversation/:conversationId", authenticateToken, validateEditConversationTitle, chatBotController.editConversationTitle);
-app.delete("/api/chatConversation/:conversationId", authenticateToken, chatBotController.deleteChatConversation);
-
-// app.get("/api/chatbot/:conversationId", chatBotController.fetchChatHistory);
-// app.post("/api/chatbot/:conversationId", validateAddChatHistory, chatBotController.postUserInput);
-
-// app.get("/api/chatConversation/:userId", chatBotController.fetchChatConversationsByUserId);
-// app.post("/api/chatConversation/:userId", validateAddNewConversation, chatBotController.addNewConversation);
-// app.put("/api/chatConversation/:conversationId", validateEditConversationTitle, chatBotController.editConversationTitle);
-// app.delete("/api/chatConversation/:conversationId", chatBotController.deleteChatConversation);
-
-//Routes for admin accounts
-app.get("/admin", adminController.getAllAdminUsers);
-app.get("/admin/:adminId", adminController.getAdminById);
-app.post("/admin", validateAdmin, adminController.createAdminUser);
-app.put("/admin/:adminId", validateAdmin, adminController.updateAdminUser);
-app.delete("/admin/:adminId", adminController.deleteAdminById);
-
-// Routes for articles
 app.get('/article', async (req, res) => {
     res.sendFile(path.join(__dirname, "/public/html/article.html"));
 });
@@ -273,6 +158,104 @@ app.get('/profile', async (req, res) => {
     res.sendFile(path.join(__dirname, "/public/html/profile.html"));
 });
 
+// Routes
+//Routes for users
+app.get("/users", userController.getAllUsers);
+app.get("/users/:userId", userController.getUserByUserId);
+app.post("/users", validateUser, userController.createUser);
+app.put("/users/:userId", validateUser, userController.updateUser);
+app.delete("/users/:userId", userController.deleteUserById);
+
+//Route for user sign up
+app.post("/signup", userController.createUser);
+
+//Routes for user login to login user
+app.post('/userlogin', userController.loginUser);
+app.post('/companylogin', userController.loginUser);
+app.post('/adminlogin', adminController.loginUser);
+
+
+app.post('/createadmin', adminController.createAdminUser);
+
+
+//Routes for events
+app.post('/create-event', authenticateToken, validateEvent, EventController.createEvent);
+
+// Serve protected.html for /events route, i used this for the js to authenticate before serving
+app.get('/events', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/html/protected.html'));
+});
+// myEvents.html page
+app.get('/api/events/userEvents', authenticateToken, EventController.getEventsByUserId);
+
+app.get('/myEvents', async(req, res) => {res.sendFile(path.join(__dirname + "/public/html/myEvents.html"))});
+
+// events-content is the route for the events page, so basically after protected.html is served, the js will fetch the events-content with the token, the middleware can then authenticate it.
+app.get('/events-content', specialAuthenticateToken, EventController.serveEventsContent);
+app.post('/api/:eventId/signup', authenticateToken, EventController.signUserUp);
+app.get("/api/events", EventController.getAllEvents);
+app.get("/api/events/:eventId", EventController.getEventById);
+app.post('/api/events', authenticateToken, validateEvent, EventController.createEvent);
+app.patch('/api/:eventId/leave', authenticateToken, EventController.updateEventAttendance);
+app.delete("/api/events/:eventId/kick/:userId", authenticateToken, (req, res, next) => {
+    EventController.updateEventAttendance(req, res, next);
+  });
+app.delete("/api/events/:eventId", authenticateToken, EventController.deleteEvent);
+
+//paypal stuff
+app.post('/api/events/:eventId/authorize', authenticateToken, eventPaymentController.authorizePayment);
+app.post('/api/events/:eventId/capture', authenticateToken, eventPaymentController.capturePayment);
+app.put('/api/payments/capture/:eventId', authenticateToken, eventPaymentController.capturePayment);
+
+// refreshToken Routes
+app.delete('/refreshToken', (req, res) => { refreshToken.removeToken(req, res) });
+app.get("/refreshToken", async(req, res) => {  refreshToken.refreshToken(req, res)});    
+app.post("/refreshToken", async(req, res) => {refreshToken.addToken(req, res)});
+
+// Game routes
+app.get('/game', async(req, res) => { res.sendFile(path.join(__dirname + "/public/html/game.html")) });
+app.post('/save-game', authenticateToken,validateGame, gameController.saveGame);
+app.post('/save-game2', authenticateToken, validateGame, gameController.saveGame2);
+app.get('/get-games', authenticateToken, gameController.getGames);
+
+// Highscore routes
+app.get('/get-high-scores', authenticateToken, highscoreController.getHighScores);
+app.post('/create-high-score', authenticateToken, highscoreController.createHighScore);
+//Ends here
+
+
+// Backend routes for comments
+app.get("/api/article/comment/:commentId", articleCommentController.getArticleCommentById);
+app.get("/api/article/:articleId/comments/latest", articleCommentController.getAllCommentsFromArticleIdByLatest);
+app.get("/api/article/:articleId/comments/relevance", articleCommentController.getAllCommentsFromArticleIdByRelevance);
+app.post("/api/article/:articleId/comments", validateComment, articleCommentController.createArticleComment);
+app.put("/api/article/:articleId/comments", validateComment, articleCommentController.updateArticleCommentContent);
+app.delete("/api/article/:articleId/comments", articleCommentController.deleteArticleComment);
+
+app.get("/api/event/comment/:commentId", eventCommentController.getEventCommentById);
+app.get("/api/event/:eventId/comments/latest", eventCommentController.getAllCommentsFromEventIdByLatest);
+app.get("/api/event/:eventId/comments/relevance", eventCommentController.getAllCommentsFromEventIdByRelevance);
+app.post("/api/event/:eventId/comments", validateComment, eventCommentController.createEventComment);
+app.put("/api/event/:eventId/comments", validateComment, eventCommentController.updateEventCommentContent);
+app.delete("/api/event/:eventId/comments", eventCommentController.deleteEventComment);
+
+// Backend routes for chatbot
+app.get("/api/chatbot/:conversationId", authenticateToken,  chatBotController.fetchChatHistory);
+app.post("/api/chatbot/:conversationId", authenticateToken, validateAddChatHistory, chatBotController.postUserInput);
+
+app.get("/api/chatConversation/:userId", authenticateToken, chatBotController.fetchChatConversationsByUserId);
+app.post("/api/chatConversation/:userId", validateEditConversationTitle, chatBotController.addNewConversation);
+app.put("/api/chatConversation/:conversationId", authenticateToken, validateEditConversationTitle, chatBotController.editConversationTitle);
+app.delete("/api/chatConversation/:conversationId", authenticateToken, chatBotController.deleteChatConversation);
+
+//Routes for admin accounts
+app.get("/admin", adminController.getAllAdminUsers);
+app.get("/admin/:adminId", adminController.getAdminById);
+app.post("/admin", validateAdmin, adminController.createAdminUser);
+app.put("/admin/:adminId", validateAdmin, adminController.updateAdminUser);
+app.delete("/admin/:adminId", adminController.deleteAdminById);
+
+// Routes for articles
 app.get("/api/article", articleController.getAllArticles);
 app.post("/create-blog", articleController.createArticle);
 app.post("/api/article", articleController.createArticle);
@@ -287,23 +270,6 @@ app.get('/api/article/title', articleController.getArticleByTitle);
 app.put("/api/article/id/:articleId", articleController.updateArticle);
 app.delete("/api/article/id/:articleId", articleController.deleteArticle);
 
-// app.get('/api/profile', authenticateToken, (req, res) => {
-//     // Fetch user data from your database based on req.user.id
-//     const userId = req.user.id;
-//     // Example SQL query:
-//     // const query = `SELECT name, email, profilePicture FROM users WHERE id = ${userId}`;
-    
-//     // Replace with your actual database query
-//     const userData = {
-//         name: 'User Name',
-//         email: 'user@example.com',
-//         profilePicture: './images/default-profile-user.jpg'
-//     };
-
-//     res.json(userData);
-// });
-
-
 app.get('/api/profile/:userId', userController.getUserProfileByUserId);
 app.get('/api/profilePicture/:userId', userController.fetchProfilePicture);
 app.post('/api/uploadProfilePicture/:userId', authenticateToken, upload.single('profilePicture'), userController.uploadProfilePicture);
@@ -314,13 +280,17 @@ app.post('/api/saveAboutInfo/:userId', authenticateToken, userController.saveAbo
 app.get('/api/adminProfile/:adminId', adminController.getAdminProfileByAdminId);
 app.get('/api/adminProfilePicture/:adminId', adminController.fetchAdminProfilePicture);
 app.post('/api/uploadAdminProfilePicture/:adminId', authenticateToken, upload.single('profilePicture'), adminController.uploadProfilePicture);
+
+// Swagger API Documentation
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.listen(PORT, async () => {
     try {
         // Await to connect to the database
         await sql.connect(dbConfig);
         console.log("Database connection established successfully");
-        console.log(`Your server is running on http://localhost:${PORT}/`)
+        console.log(`Your server is running on http://localhost:${PORT}/`);
+        console.log(`Your server is running on http://localhost:${PORT}/api-docs/`);
     } catch(err) {
         console.error("Database connection error:", err);
         // Terminate the application with an error code (optional)
--- BED_Assignment/swagger.js
+++ BED_Assignment/swagger.js
@@ -1,23 +1,15 @@
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require("swagger-autogen")();
 
 const doc = {
   info: {
-    title: 'My API',
-    description: 'Description of your API',
+    title: "My API",
+    description: "Description of your API",
   },
-  host: 'localhost:3001',
+  host: "localhost:3001", 
   schemes: ['http'],
 };
 
-const outputFile = './swagger-output.json';
-const endpointsFiles = ['./app.js'];
+const outputFile = "./swagger-output.json"; 
+const endpointsFiles = ["./app.js"]; 
 
-swaggerAutogen(outputFile, endpointsFiles, doc)
-  .then(() => {
-    console.log('Swagger JSON file created successfully.');
-  })
-  .catch((err) => {
-    console.error('Error generating Swagger JSON file:', err);
-  });
-
-console.log('Swagger generation process started...');
+swaggerAutogen(outputFile, endpointsFiles, doc);
